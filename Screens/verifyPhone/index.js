6400c15ea3467bbb0c090989f3cefe9b3e803ff1
mildk123 <mildk123@gmail.com>
2018-12-08 16:28:27 +0500

    Added Verify Phone

--- Screens/verifyPhone/index.js
+++ Screens/verifyPhone/index.js
@@ -1,14 +1,71 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { StyleSheet, View, Picker } from "react-native";
+
+import { Text, Button } from "react-native-elements";
 
 class verifyPhone extends Component {
+  static navigationOptions = {
+    header: null
+  };
+
   render() {
     return (
-      <View>
-        <Text> verifyPhone </Text>
+      <View style={styles.container}>
+        <View style={{ padding: 40 }}>
+          <Text h4 style={styles.verify}>
+            Verify your phone number
+          </Text>
+        </View>
+
+        <View style={{ alignContent: "center", justifyContent: "center" }}>
+          <Text style={styles.textContainer}>
+            WhatsApp will send an SMS message (carrier charges may apply) to
+            verify your phone number. Enter your country code and phone number:
+          </Text>
+
+          <View>
+            <Picker
+              style={{ height: 50, width: 100 }}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ language: itemValue })
+              }
+            >
+              <Picker.Item label="Java" value="java" />
+              <Picker.Item label="JavaScript" value="js" />
+            </Picker>
+          </View>
+        </View>
       </View>
-    )
+    );
   }
 }
 
-export default verifyPhone
+export default verifyPhone;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#FFFFFF"
+  },
+  verify: {
+    color: "#25D366"
+  },
+  image: {
+    width: 420,
+    height: "100%",
+    maxHeight: 520,
+    borderRadius: 100
+  },
+  textContainer: {
+    color: "grey",
+    textAlign: "center",
+    fontSize: 13,
+    padding: 25,
+    paddingTop: 0
+  },
+  spanLinks: {
+    fontSize: 12,
+    color: "#34B7F1"
+  }
+});
--- navigation/navigator.js
+++ navigation/navigator.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { createStackNavigator } from 'react-navigation'
-import WelcomeScreen from '../Screens/Welcome'
+// import WelcomeScreen from '../Screens/Welcome'
 import verifyPhone from '../Screens/verifyPhone'
 
 export default createStackNavigator({
-    Welcome: WelcomeScreen,
+    // Welcome: WelcomeScreen,
     verifyPhone : verifyPhone
   });
